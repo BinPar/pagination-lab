d8eabcd0ba015fb8f12029c48870c1da521a1b82
Ignacio Ferro Picón <nacho@binpar.com>
2020-08-10 21:26:31 +0200

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,22 @@
 import * as log from 'loglevel';
 
-const currentFontSize = 18;
+let currentFontSize = 18;
 
 const recalculateColumnConfig = (): void => {
-  const labelsContainer = document.body.querySelector('body > .labelsForEveryPage');
-  document.body.querySelectorAll('body > .labelsForEveryPage > .label').forEach((label): void => {
-    label.remove();
-  });
+  const labelsContainer = document.body.querySelector(
+    'body > .labelsForEveryPage',
+  );
+  document.body
+    .querySelectorAll('body > .labelsForEveryPage > .label')
+    .forEach((label): void => {
+      label.remove();
+    });
   const windowWidth = window.innerWidth;
   const vh = window.innerHeight;
   document.documentElement.style.setProperty('--totalChapterWidth', '0');
   document.documentElement.style.setProperty('--vh', `${vh}px`);
   document.documentElement.style.setProperty('--windowWidth', `${0}px`);
-  document.documentElement.style.setProperty(
-    '--totalColumnWidth',
-    `0`,
-  );
+  document.documentElement.style.setProperty('--totalColumnWidth', `0`);
   const columnWidth =
     document.getElementById('totalColumnWidthCalculator')?.clientWidth || 1;
   const columnsInPageWidth = Math.floor(windowWidth / columnWidth);
@@ -24,9 +25,14 @@ const recalculateColumnConfig = (): void => {
     '--totalColumnWidth',
     `${totalColumnWidth}px`,
   );
-  const totalColumns = Math.round(window.document.body.scrollWidth / totalColumnWidth);
-  const totalChapterWidth = totalColumnWidth  * totalColumns;
-  document.documentElement.style.setProperty('--totalChapterWidth', `${totalChapterWidth}px`);
+  const totalColumns = Math.round(
+    window.document.body.scrollWidth / totalColumnWidth,
+  );
+  const totalChapterWidth = totalColumnWidth * totalColumns;
+  document.documentElement.style.setProperty(
+    '--totalChapterWidth',
+    `${totalChapterWidth}px`,
+  );
   for (let column = 1; column <= totalColumns; column++) {
     const columnDiv = document.createElement('div');
     columnDiv.className = 'label';
@@ -35,13 +41,38 @@ const recalculateColumnConfig = (): void => {
     columnDiv.appendChild(label);
     labelsContainer?.appendChild(columnDiv);
   }
-}
+};
 
-const onWindowLoad = (): void => {  
+const onWindowLoad = (): void => {
   document.documentElement.style.setProperty(
     '--fontSize',
     `${currentFontSize}px`,
   );
+  const increaseFontButton = document.body.querySelector(
+    'body > .buttons > .increaseFontButton',
+  );
+  const decreaseFontButton = document.body.querySelector(
+    'body > .buttons > .decreaseFontButton',
+  );
+  const updateFontInfo = (): void => {
+    document.documentElement.style.setProperty(
+      '--fontSize',
+      `${currentFontSize}px`,
+    );
+    recalculateColumnConfig();
+  };
+  increaseFontButton?.addEventListener('click', (): void => {
+    if (currentFontSize < 100) {
+      currentFontSize += 2;
+      updateFontInfo();
+    }
+  });
+  decreaseFontButton?.addEventListener('click', (): void => {
+    if (currentFontSize > 4) {
+      currentFontSize -= 2;
+      updateFontInfo();
+    }
+  });
   recalculateColumnConfig();
   setTimeout(recalculateColumnConfig, 1000);
 };
