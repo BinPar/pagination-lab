51fde58eb611685fd30beb1740ae6438bb4999f8
Ignacio Ferro Picón <nacho@binpar.com>
2020-08-10 21:16:48 +0200

    fix by now

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,50 @@
 import * as log from 'loglevel';
 
-const onWindowLoad = (): void => {
-  // eslint-disable-next-line no-console
-  log.warn('Pagination initialized!');
+const currentFontSize = 18;
+
+const recalculateColumnConfig = (): void => {
+  const labelsContainer = document.body.querySelector('body > .labelsForEveryPage');
+  document.body.querySelectorAll('body > .labelsForEveryPage > .label').forEach((label): void => {
+    label.remove();
+  });
+  const windowWidth = window.innerWidth;
   const vh = window.innerHeight;
+  document.documentElement.style.setProperty('--totalChapterWidth', '0');
   document.documentElement.style.setProperty('--vh', `${vh}px`);
-  const chapterWidth = document.body.scrollWidth;  
-  document.documentElement.style.setProperty('--chapterWidth', `${chapterWidth}px`);
+  document.documentElement.style.setProperty('--windowWidth', `${0}px`);
+  document.documentElement.style.setProperty(
+    '--totalColumnWidth',
+    `0`,
+  );
+  const columnWidth =
+    document.getElementById('totalColumnWidthCalculator')?.clientWidth || 1;
+  const columnsInPageWidth = Math.floor(windowWidth / columnWidth);
+  const totalColumnWidth = windowWidth / columnsInPageWidth;
+  document.documentElement.style.setProperty(
+    '--totalColumnWidth',
+    `${totalColumnWidth}px`,
+  );
+  const totalColumns = Math.round(window.document.body.scrollWidth / totalColumnWidth);
+  const totalChapterWidth = totalColumnWidth  * totalColumns;
+  document.documentElement.style.setProperty('--totalChapterWidth', `${totalChapterWidth}px`);
+  for (let column = 1; column <= totalColumns; column++) {
+    const columnDiv = document.createElement('div');
+    columnDiv.className = 'label';
+    const label = document.createElement('p');
+    label.innerText = `${column}`;
+    columnDiv.appendChild(label);
+    labelsContainer?.appendChild(columnDiv);
+  }
 }
 
-window.addEventListener("load", onWindowLoad);
+const onWindowLoad = (): void => {  
+  document.documentElement.style.setProperty(
+    '--fontSize',
+    `${currentFontSize}px`,
+  );
+  recalculateColumnConfig();
+  setTimeout(recalculateColumnConfig, 1000);
+};
+
+window.addEventListener('load', onWindowLoad);
+window.addEventListener('resize', recalculateColumnConfig);
