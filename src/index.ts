88bc628e4cdfdc63e2e81c696148de9fb07de564
Ignacio Ferro Picón <nacho@binpar.com>
2020-08-10 13:29:10 +0200

    Initial Load

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+const onWindowLoad = (): void => {
+  // eslint-disable-next-line no-console
+  console.log('Pagination initialized!');
+}
+window.addEventListener("load", onWindowLoad);
---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import * as liveServer from 'live-server';
+
+const params: liveServer.LiveServerParams = {
+  port: 3000,
+  root: './web',
+  open: false,
+  wait: 0,
+  mount: [['/js', './dist']],
+  logLevel: 2,
+  file: 'index.html',
+};
+
+liveServer.start(params);
