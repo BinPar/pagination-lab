50cb2ddea45eb3473ea95180adcbd72e09033301
Ignacio Ferro Picón <nacho@binpar.com>
2020-08-24 09:56:22 +0200

    fix

--- src/recalculateColumnConfig.ts
+++ src/recalculateColumnConfig.ts
@@ -59,86 +59,90 @@ const recalculateColumnConfig = (
 
   const pagesDict: PageNumberMark[] = [];
   const pagesPerColumn: string[] = [];
+  let setScrollTo = 0;
+  if (settings.verticalScroll) {
+    // TODO: Store vertical pages marker array
+  } else {
+    const { scrollLeft } = document.body;
+    document.body
+      .querySelectorAll('body > .zoomPanel > .chapterWrapper [data-page]')
+      .forEach((item): void => {
+        const element = item as HTMLElement;
+        const rects = element.getClientRects();
+        let left = rects[0].x;
+        if (!settings.readMode) {
+          left -= (window.innerWidth / 2) * 0.25;
+          left += scrollLeft;
+          left /= 0.75;
+        } else {
+          left += scrollLeft;
+        }
 
-  const { scrollLeft } = document.body;
-  document.body
-    .querySelectorAll('body > .zoomPanel > .chapterWrapper [data-page]')
-    .forEach((item): void => {
-      const element = item as HTMLElement;
-      const rects = element.getClientRects();
-      let left = rects[0].x;
+        if (element.dataset.page) {
+          pagesDict.push({
+            left,
+            page: element.dataset.page,
+          });
+        }
+      });
+
+    let currentPage = pagesDict.length ? pagesDict[0].page : '';
+    let nextPage = '';
+    if (newSettings.currentPage === '') {
+      newSettings.currentPage = currentPage;
+    }
+
+    let pageSet = false;
+    setScrollTo = scrollingElement.scrollLeft;
+    for (let column = 1; column <= totalColumns; column++) {
+      let relativeColumnWidth = totalColumnWidth;
       if (!settings.readMode) {
-        left -= (window.innerWidth / 2) * 0.25;
-        left += scrollLeft;
-        left /= 0.75;
-      } else {
-        left += scrollLeft;
+        relativeColumnWidth *= 0.75;
       }
-
-      if (element.dataset.page) {
-        pagesDict.push({
-          left,
-          page: element.dataset.page,
-        });
-      }
-    });
-
-  let currentPage = pagesDict.length ? pagesDict[0].page : '';
-  let nextPage = '';
-  if (newSettings.currentPage === '') {
-    newSettings.currentPage = currentPage;
-  }
-
-  let pageSet = false;
-  let setScrollTo = scrollingElement.scrollLeft;
-  for (let column = 1; column <= totalColumns; column++) {
-    let relativeColumnWidth = totalColumnWidth;
-    if (!settings.readMode) {
-      relativeColumnWidth *= 0.75;
-    }
-    const maxLeft = column * totalColumnWidth;
-    if (column === 1 || column > totalColumns - 1) {
-      currentPage = '';
-    } else if (pagesDict.length && pagesDict[0].left < maxLeft) {
-      currentPage = pagesDict[0].page;
-      nextPage = currentPage;
-      while (pagesDict.length && pagesDict[0].left < maxLeft) {
-        if (!pageSet && newSettings.currentPage === pagesDict[0].page) {
-          if (currentPage === newSettings.currentPage) {
-            pageSet = true;
+      const maxLeft = column * totalColumnWidth;
+      if (column === 1 || column > totalColumns - 1) {
+        currentPage = '';
+      } else if (pagesDict.length && pagesDict[0].left < maxLeft) {
+        currentPage = pagesDict[0].page;
+        nextPage = currentPage;
+        while (pagesDict.length && pagesDict[0].left < maxLeft) {
+          if (!pageSet && newSettings.currentPage === pagesDict[0].page) {
+            if (currentPage === newSettings.currentPage) {
+              pageSet = true;
+            }
+            setScrollTo = column * relativeColumnWidth - relativeColumnWidth;
           }
-          setScrollTo = column * relativeColumnWidth - relativeColumnWidth;
-        }
-        const removed = pagesDict.shift();
-        if (removed) {
-          nextPage = removed.page;
+          const removed = pagesDict.shift();
+          if (removed) {
+            nextPage = removed.page;
+          }
         }
       }
-    }
-    if (currentPage === '-') {
-      currentPage = '';
-    }
-    pagesPerColumn.push(currentPage);
-    const columnDiv = document.createElement('div');
-    columnDiv.className = 'label';
-    const label = document.createElement('p');
-    label.innerText = `${currentPage}`;
-    columnDiv.appendChild(label);
-    labelsContainer?.appendChild(columnDiv);
-    const isScrollSnap = (column - 2) % columnsInPageWidth === 0;
-    if (isScrollSnap && currentPage) {
-      const scrollSnapDiv = document.createElement('div');
-      scrollSnapDiv.className = 'scrollSnap';
-      if (newSettings.readMode) {
-        scrollSnapDiv.style.left = `${(column - 1) * totalColumnWidth}px`;
-      } else {
-        scrollSnapDiv.style.left = `${
-          (column - 1) * (totalColumnWidth * 0.75)
-        }px`;
+      if (currentPage === '-') {
+        currentPage = '';
       }
-      pageSnapsContainer?.appendChild(scrollSnapDiv);
+      pagesPerColumn.push(currentPage);
+      const columnDiv = document.createElement('div');
+      columnDiv.className = 'label';
+      const label = document.createElement('p');
+      label.innerText = `${currentPage}`;
+      columnDiv.appendChild(label);
+      labelsContainer?.appendChild(columnDiv);
+      const isScrollSnap = (column - 2) % columnsInPageWidth === 0;
+      if (isScrollSnap && currentPage) {
+        const scrollSnapDiv = document.createElement('div');
+        scrollSnapDiv.className = 'scrollSnap';
+        if (newSettings.readMode) {
+          scrollSnapDiv.style.left = `${(column - 1) * totalColumnWidth}px`;
+        } else {
+          scrollSnapDiv.style.left = `${
+            (column - 1) * (totalColumnWidth * 0.75)
+          }px`;
+        }
+        pageSnapsContainer?.appendChild(scrollSnapDiv);
+      }
+      currentPage = nextPage;
     }
-    currentPage = nextPage;
   }
   if (updateScroll) {
     document.body.scrollTo(setScrollTo, 0);
